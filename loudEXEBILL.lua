e832bf1b79d3b25d47757608601132a9bbc7ef55
BILLBILOVICH <130781032+BILLBILOVICH@users.noreply.github.com>
2023-04-14 19:24:15 +0300

    Create loudEXEBILL.lua

---
+++ loudEXEBILL.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 6403373529 or game.PlaceId == 9015014224 or game.PlaceId == 11520107397 or game.PlaceId == 9431156611 then
+    if game.PlaceId == 9431156611 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BILLBILOVICH/BILLHACKINEXE.lua/main/exeBILLSLAPROYAL.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/BILLBILOVICH/BILLHACKINEXE.lua/main/exeBILLSLAPBATTLE"))()
+    end
+end
